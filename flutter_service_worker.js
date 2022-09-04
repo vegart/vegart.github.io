22aac353428158e9227cb35eab7cc18a7a79547c
vegart <dhyj777@gmail.com>
2022-09-05 01:28:54 +0900

    triv: style changes

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -32,9 +32,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "a8811fa31f27426272a2f690b32b26bc",
 "icons/Icon-maskable-192.png": "3f7b0deadf47428a5f279295c9df1995",
 "icons/Icon-maskable-512.png": "a8811fa31f27426272a2f690b32b26bc",
-"index.html": "481f25978515083ac8532fb2ed9c425c",
-"/": "481f25978515083ac8532fb2ed9c425c",
-"main.dart.js": "059d4ab531f546d033a67af7e7e72d2c",
+"index.html": "c91595ea171e110922094d58e000da47",
+"/": "c91595ea171e110922094d58e000da47",
+"main.dart.js": "25eb47f3128b64ef6f8989de8faaf2fb",
 "manifest.json": "472097ce02e503933830234f9935bc63",
 "version.json": "45e496a31ba528a34576b170b75a0896"
 };
